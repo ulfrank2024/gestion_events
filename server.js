0b593f5dd7a9d424a98847233cf84e1ecd314915
ulfrank2024 <2724548@collegelacite.ca>
2025-04-02 00:41:59 -0400

    Ajout des variables d'environnement

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.use((req, res, next) => {
 
 // Routes principales
 app.get("/", async (req, res) => {
-    res.render("ListeEvenements", {
+    res.render("listeEvenements", {
         titre: "Accueil | EvenementScolaire",
         style: ["/css/liste_evenement.css", "/css/listeevenment.css"],
         script: ["/js/lisedesEvenement.js", "/js/itemsCathegorie.js"],
